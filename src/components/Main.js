47f78f49229d110decb6064e78cd519d144e89a3
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2021-01-08 12:49:27 +1100

    search added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import "./Main.css"
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 
 function Main() {
+    const classes = useStyles();
+    
+
     return (
         <div>
-            
+            <div className="search">
+                <form className={classes.root} noValidate autoComplete="off">
+                    <TextField className="textfield" id="outlined-basic" label="Search!" variant="outlined" />
+                </form>
+            </div>
         </div>
     )
 }
