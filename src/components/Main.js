abf80702bb76bbf25fd2ca9a213a65b88986980d
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2021-01-09 17:35:50 +1100

    filtering working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import ResultList from "./ResultList"
 import SearchForm from "./SearchForm"
-import TextField from '@material-ui/core/TextField';
 import "./Main.css"
 import API from "../utils/API"
 
 class Main extends Component {
   state = {
     search:"",
-    results: []
+    results: [],
+    filtered: []
   };
 
   // When this component mounts, search the Giphy API for pictures of kittens
@@ -16,26 +16,27 @@ class Main extends Component {
     API.search()
       .then(res => this.setState({ results: res.data.results}))
       .catch(err => console.log(err));
+
   } 
 
   handleInputChange = event => {
-    console.log(event)
-
-    //console.log(filtered)
+    this.setState({search: event.target.value})
   } 
-    
 
-
+  
   render() {
+    const filteredList = this.state.results.filter((item) => {
+      let values = item.name.first + item.name.last;
+      return values.indexOf(this.state.search) !== -1
+    })
  
     return (
       <div>
           <div className="search">
             <SearchForm handleInputChange={this.handleInputChange}/>
           </div>
-        
-            <ResultList results={this.state.results}/>
-     
+           
+            <ResultList results={filteredList}/> 
       </div>
     );
   }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
 function SearchForm(props) {
+
     return (
         <div>
             <form className="root">
                   <input 
+                    value={props.search}
                     onChange = {props.handleInputChange}
                     className="textfield" 
                     id="outlined-basic" 
                     label="Search!" 
                     variant="outlined"
                     type="search"
-                    value={props.search}
+                    
                   />
               </form>
         </div>
